1a684553d90ee02cdcfbaa7ae13b3803b4cb8f5a
Oktovianus Irvan <tc.vanz@yahoo.com>
2019-03-08 22:37:34 +0700

    Finished with template

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-    echo "Hello worlds";
-?>
+<?php   include('core/core.php'); ?>
+
+<?php   include('includes/header.php'); ?>
+
+<!-- Here's where your content begins -->
+    <h1>Hello, world!</h1>
+    <a href="intro.php">Coba kesini</a>
+<!-- Here's where your contents ends -->
+
+<?php   include('includes/footer.php'); ?>
